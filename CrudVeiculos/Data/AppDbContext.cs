d6ec4056860e519359a4b85946fb5a0086983a4d
Yhuri Gross <yhurigross@gmail.com>
2024-10-08 19:47:01 -0300

    Configuração do banco de dados
    Configurando o banco de dados para a aplicação

---
+++ CrudVeiculos/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudVeiculos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
--- CrudVeiculos/Program.cs
+++ CrudVeiculos/Program.cs
@@ -9,6 +9,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configuração do banco de dados
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
